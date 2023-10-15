ebc3ebb12be2afb98ecb0a2560e8f1af641b71da
josemidev24 <josemidev24@gmail.com>
2023-10-15 01:08:12 -0500

    feat(poo): ✨ Add Encapsulation

---
+++ 05_POO/03_Encapsulation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Course_CSharp._05_POO;
+
+/*
+ * Encapsulamiento en C#
+ * El encapsulamiento nos permite proteger los miembros (métodos y propiedades) de una clase y definir
+ * cómo se accede a ellos desde otras partes del programa.
+ * El encapsulamiento se logra mediante el uso de Modificadores de Acceso, como:
+ * public, private, protected, entre otros.
+*/
+public class Animal
+{
+    // Atributo privado
+    private string _nombre;
+
+
+    // 1era Forma para declarar un Getter & Setter
+    public string Nombre
+    {
+        get => _nombre;
+
+        set => _nombre = value;
+    }
+
+    // 2da Forma para declarar un Getter & Setter
+    public string Especie { get; set; }
+
+
+    public void ObtenerInfo()
+    {
+        Console.WriteLine($"Hola soy un {Especie} y me llamo {Nombre}!");
+    }
+}
+
+public class Perro : Animal
+{
+    public Perro()
+    {
+        Especie = "perro";
+    }
+
+    public void Ladrar()
+    {
+        Console.WriteLine("Guau guau!");
+    }
+}
---
+++ 05_POO/POO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Course_CSharp._05_POO;
+
+public class POO
+{
+    public POO()
+    {
+        // Creando una instancia de la clase Persona
+        Persona persona1 = new Persona("Jhon Doe", 42);
+        Persona persona2 = new Persona("Mac Miller", 26);
+
+        // Llamando al método Saludar()
+        persona1.Saludar();
+        persona2.Saludar();
+
+
+        Console.WriteLine("************** HERENCIA *******************");
+
+
+        Carro carro1 = new Carro("Tesla", "X", "rojo");
+        Carro carro2 = new Carro("Audi", "Q3", "blanco");
+
+        // Lamando al método heredado de la clase Padre
+        carro1.Acelerar();
+
+        // Método sobreescrito
+        carro1.ObtenerDatos();
+
+        carro2.Girar("derecha");
+
+        Moto moto1 = new Moto("Ducati", "Monster", 1500);
+        Moto moto2 = new Moto("Suzuki", "A1", 1000);
+
+        moto1.Acelerar();
+
+        moto2.ObtenerDatos();
+        moto2.HacerAcrobacia();
+
+
+        Console.WriteLine("************** Encapsulamiento *******************");
+
+
+        Animal loro = new Animal()
+        {
+            Nombre = "Otto",
+            Especie = "loro"
+        };
+
+        loro.ObtenerInfo();
+
+        Perro perro = new Perro() { Nombre = "Teddy" };
+
+        perro.ObtenerInfo();
+        perro.Ladrar();
+    }
+}
