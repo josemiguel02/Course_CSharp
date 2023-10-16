c668765e3c04f1a1ef26cf77a77d606cf29ccdda
José Domínguez P <jdominguez@viamatica.me>
2023-10-16 13:57:12 -0500

    feat(generics): ✨ Add Generics Section

---
+++ 06_Generics/01_ClassGeneric.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Course_CSharp._06_Generics;
+
+/*
+ * Genéricos en C#
+ * Los genéricos introducen el concepto de parámetros de tipo a .NET, lo que le permite diseñar clases y métodos
+ * que aplazan la especificación de uno o varios tipos hasta que el código de cliente declare y cree una
+ * instancia de la clase o el método.
+ *
+*/
+
+// Clase Generica
+// Para convertir una clase en generica debemos usar la siguiente sintaxis:
+// public class NombreClase<T> {}
+public class ArregloGenerico<T>
+{
+    private T[] _arreglo;
+
+    public ArregloGenerico(short longitud)
+    {
+        _arreglo = new T[longitud];
+    }
+
+    public void Agregar(int indice, T item)
+    {
+        _arreglo[indice] = item;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(" - ", _arreglo);
+    }
+}
---
+++ 06_Generics/Generics.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Course_CSharp._06_Generics;
+
+public class Generics
+{
+    public Generics()
+    {
+        // Clase Generica
+        ArregloGenerico<string> arregloGen = new ArregloGenerico<string>(2);
+
+        arregloGen.Agregar(0, "Item 1");
+        arregloGen.Agregar(1, "Item 2");
+
+        Console.WriteLine(arregloGen.ToString());
+
+
+        // Propiedades y Métodos Genéricos
+        ResultadoOperacion<int> operacion = new ResultadoOperacion<int>();
+
+        operacion.AgregarResultado(200);
+
+        Console.WriteLine(operacion.ObtenerResultado());
+    }
+}
